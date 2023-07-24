25d7c2deba8f85d6baf0140b0ad093549b9c4454
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-07-24 00:29:46 +0000

    final touches

--- src/main/java/com/health/knusthealthportal/entities/Appointment.java
+++ src/main/java/com/health/knusthealthportal/entities/Appointment.java
@@ -5,52 +5,27 @@ import org.antlr.v4.runtime.misc.NotNull;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.time.LocalTime;
 import java.util.UUID;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Data
 @Entity
 @Table(name="appointments")
 public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private  UUID id;
+    private UUID id;
 
-    private  String studentName;
+    private String studentName;
 
-    private  String description;
+    private String description;
 
-    private  Date date;
+    private Date date;
 
-    private Time time;
-
-
-
-
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
+    private LocalTime time;
 }
