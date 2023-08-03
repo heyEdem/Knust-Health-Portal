ed13036867f0e315a69ad70a729c0a3eff49596e
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-08-03 22:16:25 +0000

    Miscellaneous

--- src/main/java/com/health/knusthealthportal/KnustHealthPortalApplication.java
+++ src/main/java/com/health/knusthealthportal/KnustHealthPortalApplication.java
@@ -10,55 +10,55 @@ import java.util.UUID;
 import static com.health.knusthealthportal.Roles.*;
 
 @SpringBootApplication
-public class KnustHealthPortalApplication implements CommandLineRunner {
-    private final UserRepository userRepository;
-
-    public KnustHealthPortalApplication(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+public class KnustHealthPortalApplication  {
+//    private final UserRepository userRepository;
+//
+//    public KnustHealthPortalApplication(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(KnustHealthPortalApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        User s1 = new User();
-        s1.setId(1);
-        s1.setUsername("abdul");
-        s1.setPassword("1");
-        s1.setRole(STUDENT);
+//    @Override
+//    public void run(String... args) throws Exception {
+//        User s1 = new User();
+//        s1.setId(1);
+//        s1.setUsername("abdul");
+//        s1.setPassword("1");
+//        s1.setRole(STUDENT);
+//
+//        User s2 = new User();
+//        s2.setId(2);
+//        s1.setUsername("edem");
+//        s2.setPassword("12");
+//        s2.setRole(ADMIN);
+//
+//        User s3 = new User();
+//        s3.setId(3);
+//        s3.setUsername("enchil");
+//        s3.setPassword("123");
+//        s3.setRole(STUDENT);
+//
+//
+//        User s4 = new User();
+//        s4.setId(4);
+//        s4.setUsername("prince");
+//        s4.setPassword("1234");
+//        s4.setRole(DOCTOR);
+//
+//        User s5 = new User();
+//        s5.setId(5);
+//        s5.setUsername("robert");
+//        s5.setPassword("12345");
+//        s5.setRole(STUDENT);
+//
+//        this.userRepository.save(s1);
+//        this.userRepository.save(s2);
+//        this.userRepository.save(s3);
+//        this.userRepository.save(s4);
+//        this.userRepository.save(s5);
 
-        User s2 = new User();
-        s2.setId(2);
-        s1.setUsername("edem");
-        s2.setPassword("12");
-        s2.setRole(ADMIN);
-
-        User s3 = new User();
-        s3.setId(3);
-        s3.setUsername("enchil");
-        s3.setPassword("123");
-        s3.setRole(STUDENT);
-
-
-        User s4 = new User();
-        s4.setId(4);
-        s4.setUsername("prince");
-        s4.setPassword("1234");
-        s4.setRole(DOCTOR);
-
-        User s5 = new User();
-        s5.setId(5);
-        s5.setUsername("robert");
-        s5.setPassword("12345");
-        s5.setRole(STUDENT);
-
-        this.userRepository.save(s1);
-        this.userRepository.save(s2);
-        this.userRepository.save(s3);
-        this.userRepository.save(s4);
-        this.userRepository.save(s5);
-
-    }
+    //}
 }
---
+++ src/main/java/com/health/knusthealthportal/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.health.knusthealthportal;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+            http
+                    .authorizeHttpRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll();
+        return http.build();
+    }
+}
